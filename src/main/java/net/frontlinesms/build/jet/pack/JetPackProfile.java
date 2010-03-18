a06ecabb3f90537d3e09402516f3c5a553db2583
Alex Anderson <alex@frontlinesms.com>
2010-03-18 12:47:44 +0000

    Sorted out package contents.

--- src/main/java/net/frontlinesms/build/jet/pack/JetPackProfile.java
+++ src/main/java/net/frontlinesms/build/jet/pack/JetPackProfile.java
@@ -5,9 +5,7 @@ package net.frontlinesms.build.jet.pack;
 
 import java.io.File;
 import java.io.FileFilter;
-import java.io.FilenameFilter;
 import java.io.IOException;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -31,7 +29,7 @@ public class JetPackProfile {
 	/** Property key: TODO document from template.notes.txt */
 	private static final String PROP_RESOURCEPATH = "resourcepath";
 	/** Property key: TODO document from template.notes.txt */
-	private static final String PACK_CONTENT_LINE_FORMAT = "packContent.lineFormat";
+	private static final String PROP_PACK_CONTENT_LINE_FORMAT = "packContent.lineFormat";
 	/** Property key: TODO document from template.notes.txt */
 	private static final String PROP_PACK_CONTENT = "packContent";
 	/** Property key: for {@link #productName} */
@@ -52,6 +50,9 @@ public class JetPackProfile {
 	private final File workingDirectory;
 	/** This is the directory that jars are located in. */
 	private final File jarDirectory;
+	/** This is the directory that package contents are located in. */
+	private final File packContentDirectory;
+	
 	/** Property: TODO document from template.notes.txt */
 	private final String productName;
 	/** Property: TODO document from template.notes.txt */
@@ -80,6 +81,8 @@ public class JetPackProfile {
 		assert(this.workingDirectory.exists()) : "The working directory does not exist: " + this.workingDirectory.getAbsolutePath();
 		this.jarDirectory = new File(workingDirectory, "packClasspath");
 		assert(this.jarDirectory.exists()) : "The classpath directory does not exist: " + this.jarDirectory.getAbsolutePath();
+		this.packContentDirectory = new File(workingDirectory, "packContent");
+		assert(this.packContentDirectory.exists()) : "The pack content directory does not exist: " + this.packContentDirectory.getAbsolutePath();
 		
 		this.productName = productName;
 		this.productVersion = productVersion;
@@ -97,9 +100,15 @@ public class JetPackProfile {
 		
 		props.put(PROP_WORKING_DIRECTORY, workingDirectory.getAbsolutePath());
 		
+		// Add classpath details
 		props.put(PROP_CLASSPATH_COMMANDLINE, getClasspathCommandline());
 		props.put(PROP_RESOURCEPATH, getResourcepath());
 		
+		// Add package content details
+		props.put(PROP_PACK_CONTENT, getPackContent());
+		props.put(PROP_PACK_CONTENT_LINE_FORMAT, getPackContentLineFormat());
+		
+		// Add other variables
 		props.put(PROP_PRODUCT_NAME, this.productName);
 		props.put(PROP_PRODUCT_VERSION, this.productVersion);
 		props.put(PROP_PRODUCT_VERSION_STANDARDISED, this.productVersionStandardised);
@@ -109,12 +118,44 @@ public class JetPackProfile {
 		props.put(PROP_START_MENU_PROGRAM_FOLDER_ROOT, this.startMenuProgramFolderRoot);
 		props.put(PROP_PROGRAM_FILES_HOME, this.programFilesHome);
 
-		addPlaceholders(props, PROP_PACK_CONTENT, PACK_CONTENT_LINE_FORMAT);
-		
 		return props;
 	}
 
 //> PROPERTY GENERATION
+	/** @return entry for {@link #PROP_PACK_CONTENT} */
+	private String getPackContent() {
+		return new PackContentBuilder(this.packContentDirectory).toString();
+	}
+	
+	/** @return entry for {@link #PROP_PACK_CONTENT_LINE_FORMAT} */
+	private String getPackContentLineFormat() {
+		StringBuilder bob = new StringBuilder();
+		for(File f : this.packContentDirectory.listFiles()) {
+			appendPackContentLineFormat(bob, f);
+		}
+		return bob.toString();
+	}
+	
+	private void appendPackContentLineFormat(StringBuilder bob, File f) {
+		if(f.isDirectory()) {
+			bob.append("!directory \"" + getRelativePackPath(f) + "\"\n");
+			for(File child : f.listFiles()) appendPackContentLineFormat(bob, child);
+		} else if(f.isFile()) {
+			bob.append("!file \"" + getRelativePackPath(f) + "\" \"" + f.getAbsolutePath() + "\"\n");
+		} else throw new RuntimeException("Not sure what to do with file: " + f.getAbsolutePath());
+	}
+	
+	private String getRelativePackPath(File f) {
+		String packRoot = this.packContentDirectory.getAbsolutePath();
+		String abs = f.getAbsolutePath();
+		String rel = abs.substring(packRoot.length());
+		while(rel.length()>0 && (rel.charAt(0)=='\\' || rel.charAt(0)=='/')) {
+			rel = rel.substring(1);
+		}
+		return rel;
+	}
+
+	/** @return entry for {@link #PROP_CLASSPATH_COMMANDLINE} */
 	private String getClasspathCommandline() {
 		StringBuilder bob = new StringBuilder();
 		File[] jarList = this.getJarlist();
@@ -125,7 +166,7 @@ public class JetPackProfile {
 		return bob.toString();
 	}
 	
-	/** @return entry for # */
+	/** @return entry for {@link #PROP_RESOURCEPATH} */
 	private String getResourcepath() {
 		StringBuilder bob = new StringBuilder();
 		File[] jarList = this.getJarlist();
@@ -145,14 +186,6 @@ public class JetPackProfile {
 				return file.isFile() && file.getName().endsWith(".jar");
 			}
 		});
-	}
-	
-	@Deprecated
-	private void addPlaceholders(Map<String, String> props, String... propertyKeys) {
-		for(String propertyKey : propertyKeys) {
-			String replaced = props.put(propertyKey, "[placeholder:" + propertyKey + "]");
-			assert(replaced == null) : "Property overwritten: " + propertyKey;
-		}
 	}
 
 //> STATIC FACTORIES
@@ -175,5 +208,79 @@ public class JetPackProfile {
 		assert(props.size() == 0) : "There are " + props.size() + " unused properties.";
 		
 		return packProfile;
+	}
+}
+
+class PackContentBuilder {
+	private static final String LINEEND = "\n";
+	private static final String TAB = "    ";
+	
+	private final long TIMESTAMP;
+	private int currentIndent;
+	private final StringBuilder bob = new StringBuilder(); 
+	
+	public PackContentBuilder(File root) {
+		this.TIMESTAMP = System.currentTimeMillis();
+		
+		++currentIndent;
+		for(File f : root.listFiles()) {
+			appendFile(f);
+		}
+		--currentIndent;
+		
+		assert(currentIndent == 0) : "Seem to have traversed directories wierdly: indent=" + currentIndent;
+	}
+
+	private void appendFile(File f) {
+		if(f.isDirectory()) {
+			// add opening tag for folder
+			String folderName = f.getName();
+			String folderPermissions = "0 0 4";
+			append("<folder \"" + folderName
+					+ "\" " + folderPermissions);
+			
+			// add contents of the folder
+			++currentIndent;
+			for(File child : f.listFiles()) {
+				appendFile(child);
+			}
+			--currentIndent;
+			
+			// add closing tag for folder
+			append(">end //folder");
+		} else if(f.isFile()) {
+			append("<file");
+			appendAttribute("basic_info", "\"" + f.getName() + "\" \"" + f.getAbsolutePath() + "\"");
+			appendAttribute("flags", "0 0 4");
+			appendAttribute("cachedLastModified", TIMESTAMP + "L");
+			appendAttribute("attributes", "0");
+			append(">end //file");
+		} else throw new RuntimeException("Not sure how to handle file: " + f.getAbsolutePath());
+	}
+	
+	private void indent() {
+		for(int i=0; i<currentIndent; ++i) bob.append(TAB);
+	}
+	
+	private void append(String line) {
+		indent();
+		// append content
+		bob.append(line);
+		// append line end
+		bob.append(LINEEND);
+	}
+	
+	private void appendAttribute(String name, String value) {
+		indent();
+		bob.append(TAB);
+		bob.append('!');
+		bob.append(name);
+		bob.append(' ');
+		bob.append(value);
+		bob.append(LINEEND);
+	}
+	
+	public String toString() {
+		return bob.toString();
 	}
 }
