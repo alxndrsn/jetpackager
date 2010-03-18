8f899c941fd0c8cd78409a2b4f4deadabc940fba
Alex Anderson <alex@frontlinesms.com>
2010-03-18 11:38:10 +0000

    Added classpath entry generation to packer.

--- src/main/java/net/frontlinesms/build/jet/pack/JetPackProfile.java
+++ src/main/java/net/frontlinesms/build/jet/pack/JetPackProfile.java
@@ -4,6 +4,8 @@
 package net.frontlinesms.build.jet.pack;
 
 import java.io.File;
+import java.io.FileFilter;
+import java.io.FilenameFilter;
 import java.io.IOException;
 import java.util.Collections;
 import java.util.HashMap;
@@ -48,6 +50,8 @@ public class JetPackProfile {
 //> INSTANCE PROPERTIES
 	/** This is the directory that all paths in the package configuration are relative to. */
 	private final File workingDirectory;
+	/** This is the directory that jars are located in. */
+	private final File jarDirectory;
 	/** Property: TODO document from template.notes.txt */
 	private final String productName;
 	/** Property: TODO document from template.notes.txt */
@@ -58,17 +62,33 @@ public class JetPackProfile {
 	private final String productDescription;
 	/** Property: TODO document from template.notes.txt */
 	private final String productVendor;
+	/** Property: TODO document from template.notes.txt */
+	private final String executableName;
+	/** Property: TODO document from template.notes.txt */
+	private final String startMenuProgramFolderRoot;
+	/** Property: TODO document from template.notes.txt */
+	private final String programFilesHome;
 	
 //> CONSTRUCTORS
-	private JetPackProfile(File workingDirectory, String productName,
+	private JetPackProfile(File workingDirectory,
+				String productName,
 				String productVersion, String productVersionStandardised,
-				String productDescription, String productVendor) {
+				String productDescription, String productVendor,
+				String executableName, String startMenuProgramFolderRoot,
+				String programFilesHome) {
 		this.workingDirectory = workingDirectory;
+		assert(this.workingDirectory.exists()) : "The working directory does not exist: " + this.workingDirectory.getAbsolutePath();
+		this.jarDirectory = new File(workingDirectory, "packClasspath");
+		assert(this.jarDirectory.exists()) : "The classpath directory does not exist: " + this.jarDirectory.getAbsolutePath();
+		
 		this.productName = productName;
 		this.productVersion = productVersion;
 		this.productVersionStandardised = productVersionStandardised;
 		this.productDescription = productDescription;
 		this.productVendor = productVendor;
+		this.executableName = executableName;
+		this.startMenuProgramFolderRoot = startMenuProgramFolderRoot;
+		this.programFilesHome = programFilesHome;
 	}
 	
 //> INSTANCE METHODS
@@ -76,25 +96,61 @@ public class JetPackProfile {
 		Map<String, String> props = new HashMap<String, String>();
 		
 		props.put(PROP_WORKING_DIRECTORY, workingDirectory.getAbsolutePath());
+		
+		props.put(PROP_CLASSPATH_COMMANDLINE, getClasspathCommandline());
+		props.put(PROP_RESOURCEPATH, getResourcepath());
+		
 		props.put(PROP_PRODUCT_NAME, this.productName);
 		props.put(PROP_PRODUCT_VERSION, this.productVersion);
 		props.put(PROP_PRODUCT_VERSION_STANDARDISED, this.productVersionStandardised);
 		props.put(PROP_PRODUCT_DESCRIPTION, this.productDescription);
 		props.put(PROP_PRODUCT_VENDOR, this.productVendor);
+		props.put(PROP_EXECUTABLE_NAME, this.executableName);
+		props.put(PROP_START_MENU_PROGRAM_FOLDER_ROOT, this.startMenuProgramFolderRoot);
+		props.put(PROP_PROGRAM_FILES_HOME, this.programFilesHome);
 
-		addPlaceholders(props, PROP_PACK_CONTENT, PACK_CONTENT_LINE_FORMAT,
-				PROP_RESOURCEPATH, PROP_CLASSPATH_COMMANDLINE,
-				PROP_EXECUTABLE_NAME,
-				PROP_PROGRAM_FILES_HOME, PROP_START_MENU_PROGRAM_FOLDER_ROOT);
+		addPlaceholders(props, PROP_PACK_CONTENT, PACK_CONTENT_LINE_FORMAT);
 		
 		return props;
 	}
 
-	//> PROPERTY GENERATION
+//> PROPERTY GENERATION
+	private String getClasspathCommandline() {
+		StringBuilder bob = new StringBuilder();
+		File[] jarList = this.getJarlist();
+		for (int i = 0; i < jarList.length; i++) {
+			if(i > 0) bob.append(';');
+			bob.append(jarList[i].getName());
+		}
+		return bob.toString();
+	}
+	
+	/** @return entry for # */
+	private String getResourcepath() {
+		StringBuilder bob = new StringBuilder();
+		File[] jarList = this.getJarlist();
+		for (int i = 0; i < jarList.length; i++) {
+			bob.append("            !entry 3 \"");
+			bob.append(jarList[i].getAbsolutePath());
+			bob.append("\" null\n");
+		}
+		return bob.toString();
+	}
+	
+	/** @return a list of jars in the {@link #jarDirectory} */
+	private File[] getJarlist() {
+		return this.jarDirectory.listFiles(new FileFilter() {
+			@Override
+			public boolean accept(File file) {
+				return file.isFile() && file.getName().endsWith(".jar");
+			}
+		});
+	}
+	
 	@Deprecated
 	private void addPlaceholders(Map<String, String> props, String... propertyKeys) {
 		for(String propertyKey : propertyKeys) {
-			String replaced = props.put(propertyKey, "[placehodler:" + propertyKey + "]");
+			String replaced = props.put(propertyKey, "[placeholder:" + propertyKey + "]");
 			assert(replaced == null) : "Property overwritten: " + propertyKey;
 		}
 	}
@@ -105,11 +161,14 @@ public class JetPackProfile {
 		Map<String, String> props = PropertyUtils.loadProperties(new File(profileDirectory, "pack.profile.properties"));
 		
 		JetPackProfile packProfile = new JetPackProfile(profileDirectory,
-				props.get(PROP_PRODUCT_NAME),
-				props.get(PROP_PRODUCT_VERSION),
-				props.get(PROP_PRODUCT_VERSION_STANDARDISED),
-				props.get(PROP_PRODUCT_DESCRIPTION),
-				props.get(PROP_PRODUCT_VENDOR)
+				props.remove(PROP_PRODUCT_NAME),
+				props.remove(PROP_PRODUCT_VERSION),
+				props.remove(PROP_PRODUCT_VERSION_STANDARDISED),
+				props.remove(PROP_PRODUCT_DESCRIPTION),
+				props.remove(PROP_PRODUCT_VENDOR),
+				props.remove(PROP_EXECUTABLE_NAME),
+				props.remove(PROP_START_MENU_PROGRAM_FOLDER_ROOT),
+				props.remove(PROP_PROGRAM_FILES_HOME)
 				);
 
 		// Check all properties used
